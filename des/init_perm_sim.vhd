f3b0032c828fc5b2fcf0e573189d59f0da96cae5
antznin <antonin.godard@telecom-paristech.fr>
2019-04-29 09:23:15 +0200

    Remove comments and useless file

--- des/init_perm_sim.vhd
+++ des/init_perm_sim.vhd
@@ -1,3 +1,7 @@
+-- This simulation tests the two permutations. Since the second is the inverse of the 
+-- first, it drives the output of the first into the second. Thus you can easily compare
+-- the values.
+
 use std.env.all;
 
 library ieee;
@@ -6,25 +10,34 @@ use ieee.numeric_std.all;
 use ieee.math_real.all;
 
 entity initperm_sim is
-	port ( output: out std_ulogic_vector(63 downto 0) );
+	port ( output_inv: out std_ulogic_vector(63 downto 0) );
 end entity initperm_sim;
 
 architecture sim of initperm_sim is
 
-	signal clk: std_ulogic;
-	signal sresetn: std_ulogic;
-	signal input: std_ulogic_vector(63 downto 0);
-	signal power: std_ulogic;
+	signal clk:        std_ulogic;
+	signal sresetn:    std_ulogic;
+	signal input:      std_ulogic_vector(63 downto 0);
+	signal output:     std_ulogic_vector(63 downto 0);
+	signal power:      std_ulogic;
 
 begin
 
 	perm: entity work.initp(rtl)
 	port map (
-       		clk => clk,
+       		clk     => clk,
 		sresetn => sresetn,
-		input => input,
-		output => output,
-		power => power
+		input   => input,
+		output  => output,
+		power   => power
+	);	
+	perm_inv: entity work.inv_initp(rtl)
+	port map (
+       		clk     => clk,
+		sresetn => sresetn,
+		input   => output,
+		output  => output_inv,
+		power   => power
 	);	
 
 	-- the clock
@@ -40,39 +53,27 @@ begin
 		variable seed1: positive := 1;
 		variable seed2: positive := 1;
 		variable rnd:   real;
+		variable init:  std_ulogic := '0';
 	begin
+		input <= (others => '0');
 		sresetn <= '0';
 		for i in 1 to 10 loop
 			wait until rising_edge(clk);
 		end loop;
 		sresetn <= '1';
-		for i in 1 to 1000 loop
-			uniform(seed1, seed2, rnd);
-			if rnd < 0.1 then
-				sresetn <= '0';
-			else
-				sresetn <= '1';
-			end if;
-			uniform(seed1, seed2, rnd);
-			if rnd < 0.2 then
-				power <= '0';
-			else
-				power <= '1';
-			end if;
+		for i in 1 to 10 loop
+			wait until rising_edge(clk);
+		end loop;
+		power <= '0';
+		for i in 1 to 10 loop
+			wait until rising_edge(clk);
+		end loop;
+		power <= '1';
+
+		while unsigned(input) /= 1000 loop -- Testing the first 1000 values
+			input <= std_ulogic_vector(unsigned(input) + 1);
 			wait until rising_edge(clk);
 		end loop;
 		finish;
-	end process;
-	
-	-- drive the input
-	process 
-	begin
-		input <= (others => '0');
-		for i in 1 to 1000 loop
-			for i in 1 to 10 loop
-				wait until rising_edge(clk);
-			end loop;
-			input <= std_ulogic_vector(unsigned(input) + 2000000000);
-		end loop;
 	end process;
 end architecture sim;
