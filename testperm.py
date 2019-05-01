8dc904dfad86e1111bc696cccf56e57daed244c5
antznin <antonin.godard@telecom-paristech.fr>
2019-05-01 21:19:25 +0200

    Add e and p and remove the rest

--- testperm.py
+++ testperm.py
@@ -1,28 +1,24 @@
 import sys
 input1 = sys.argv[1]
-input2 = sys.argv[2]
 
-def C(input):
-    output = input[56] + input[48] + input[40] + input[32] + input[24] + input[16] + input[8] + input[0] + input[57] + \
-    input[49] + input[41] + input[33] + input[25] + input[17] + input[9] + input[1] + input[58] + input[50] + input[42] \
-    +input[34] + input[26] + input[18] + input[10] + input[2] + input[59] + input[51] + input[43] + input[35]
+ptable = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24,\
+		14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25]
+
+etable = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14,\
+		15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26,\
+		27, 28, 29, 28, 29, 30, 31, 32, 1]
+
+def p(inp):
+    output = ''
+    for i in range(len(ptable)):
+        output += inp[ptable[i]-1]
     return output
 
-def D(input):
-    output = input[62] + input[54] + input[46] + input[38] + input[30] + input[22] +\
-    input[14] + input[6] + input[61] + input[53] + input[45] + input[37] + input[29] +\
-    input[21] + input[13] + input[5] + input[60] + input[52] + input[44] + input[36] + \
-    input[28] + input[20] + input[12] + input[4] + input[27] + input[19] + input[11] + input[3]
+def e(inp):
+    output = ''
+    for i in range(len(etable)):
+        output += inp[etable[i]-1]
     return output
 
-def pc2(inp):
-    output = \
-        inp[13] + inp[16] + inp[10] + inp[23] + inp[0] + inp[4] + inp[2] + inp[27] +\
-        inp[14] + inp[5] + inp[20] + inp[9] + inp[22] + inp[18] + inp[11] + inp[3] + inp[25] +\
-        inp[7] + inp[15] + inp[6] + inp[26] + inp[19] + inp[12] + inp[1] + inp[40] + inp[51]+\
-        inp[30] + inp[36] + inp[46] + inp[54] + inp[29] + inp[39] + inp[50] + inp[44] + \
-        inp[32] + inp[47] + inp[43] + inp[48] + inp[38] + inp[55] + inp[33] + inp[52] + \
-        inp[45] + inp[41] + inp[49] + inp[35] + inp[28] + inp[31]
-    return output
-
-print(pc2(list(reversed(input1 + input2))))
+print(p(input1))
+print(e(input1))
