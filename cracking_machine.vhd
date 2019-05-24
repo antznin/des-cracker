d20b1dee9fdc1c15e8c2d47d74a89fffc09d1c88
antznin <antonin.godard@telecom-paristech.fr>
2019-05-24 15:30:07 +0200

    Modify starting_k to signal & change conditions
    Change current_k updating conditions

--- cracking_machine.vhd
+++ cracking_machine.vhd
@@ -6,20 +6,20 @@ use work.des_pkg.all;
 
 entity cracking_machine is
 	generic (
-		starting_k: w56;
 		N:          integer
 	);
 	port (
-		clk:     in  std_ulogic;
-		sresetn: in  std_ulogic;
-		enable:  in  std_ulogic; -- "Power button"
-		p:       in  w64;
-		c:       in  w64;
-		found_k: out w56;
-		found:   out std_ulogic; -- Set to '1' if the mach in e found the key
-		k0_mw:   in  std_ulogic; -- MSB of k0 written
-		k0_lw:   in  std_ulogic; -- LSB of k0 written
-		k_req:   out w56 -- Key that may be requested
+		clk:        in  std_ulogic;
+		sresetn:    in  std_ulogic;
+		enable:     in  std_ulogic; -- "Power button"
+		p:          in  w64;
+		c:          in  w64;
+		found_k:    out w56;
+		starting_k: in  w56;
+		found:      out std_ulogic; -- Set to '1' if the mach in e found the key
+		k0_mw:      in  std_ulogic; -- MSB of k0 written
+		k0_lw:      in  std_ulogic; -- LSB of k0 written
+		k_req:      out w56 -- Key that may be requested
 	);
 end entity cracking_machine;
 
@@ -28,7 +28,7 @@ architecture rtl of cracking_machine is
 	type states_cracking is (FROZEN, RUNNING);
 
 	signal state_crack: states_cracking;
-	signal current_k:   w56 := starting_k;
+	signal current_k:   w56;
 
 begin
 
@@ -54,13 +54,15 @@ begin
 						state_crack <= RUNNING;
 						if k0_lw = '1' then
 							state_crack <= FROZEN;
+						else
+							if des(p, extend_key(current_k), true) = c then
+								state_crack <= FROZEN;
+								found       <= '1';
+								found_k     <= current_k;
+							else
+								current_k <= inc(current_k, N);
+							end if;
 						end if;
-						if des(p, extend_key(current_k), true) = c then
-							state_crack <= FROZEN;
-							found       <= '1';
-							found_k     <= current_k;
-						end if;
-						current_k <= inc(current_k, N);
 				end case;
 			end if;
 		end if;
