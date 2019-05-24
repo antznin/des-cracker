4f34b0ff6ed4a9d046fbf24719c1629af7ba4d59
antznin <antonin.godard@telecom-paristech.fr>
2019-05-24 15:34:31 +0200

    Add working cracker with simulation

--- des_cracker.vhd
+++ des_cracker.vhd
@@ -13,7 +13,7 @@ entity des_cracker is
 	);
 	port (
 		-- Clock and reset
-		clk:       in  std_ulogic;
+		clk:       in  std_ulogic; --! The main clock signal
 		sresetn:   in  std_ulogic;
 		enable :   in  std_ulogic;
 		p:         in  w64; -- plaintext, Base Address: 0x000
@@ -24,17 +24,18 @@ entity des_cracker is
 		k0_lw:     in  std_ulogic; -- LSB of k0 written
 		k_mr:      in  std_ulogic; -- MSB of k read
 		k_lr:      in  std_ulogic; -- LSB of k read
+		found:     out std_ulogic;
 		k_req:     out w56 -- Key to send in case of requests
 	);
 end entity des_cracker;
 
 architecture rtl of des_cracker is
 
-	type states_request  is (UPDATING, FREEZE);
-	signal state_req:   states_request;
+	type states_request is (UPDATING, FREEZE);
+	signal state_req: states_request;
 
-	signal found_t:   table_bit(1 to N); -- table containing all mach in e found signals
-	signal found_k_t: table56(1 to N); -- table containing all mach in e found_k signals
+	signal found_t:   table_bit(1 to N); -- table containing all machine found signals
+	signal found_k_t: table56(1 to N); -- table containing all machine found_k signals
 	signal k_req_t:   table56(1 to N); -- All requested keys
 
 begin
@@ -44,33 +45,39 @@ begin
 	begin
 		cracking_machines : entity work.cracking_machine(rtl)
 		generic map (
-			N          => N,
-			starting_k => std_ulogic_vector(unsigned(k0) + i)
+			N          => N
 		)
 		port map (
-			clk     => clk,
-			sresetn => sresetn,
-			enable  => enable,
-			p       => p,
-			c       => c,
-			found_k => found_k_t(i+1),
-			found   => found_t(i+1),
-			k0_mw   => k0_mw,
-			k0_lw   => k0_lw,
-			k_req   => k_req_t(i+1)
+			clk        => clk,
+			sresetn    => sresetn,
+			enable     => enable,
+			p          => p,
+			c          => c,
+			found_k    => found_k_t(i+1),
+			starting_k => std_ulogic_vector(unsigned(k0) + i),
+			found      => found_t(i+1),
+			k0_mw      => k0_mw,
+			k0_lw      => k0_lw,
+			k_req      => k_req_t(i+1)
 		);
 	end generate;
 
-	-- Synchronous process to reset signals
-	sync: process (clk)
+	-- As soon as found_t changes, we check if an element equals '1', thus
+	-- indicating if a key has been found. It stores the found key in k1
+	process (clk, found_t)
 	begin
 		if rising_edge(clk) then
 			if sresetn = '0' then
-				found_t   <= (others => '0');
-				found_k_t <= (others => (others => '0'));
-				k1        <= (others => '0');
-				k_req     <= (others => '0');
-				k_req_t   <= (others => (others => '0'));
+				k1    <= (others => '0');
+				found <= '0';
+			elsif enable = '1' then
+				for i in 1 to N loop
+					if found_t(i) = '1' then
+						k1 <= found_k_t(i);
+						found <= '1';
+					end if;
+					-- TODO : IRQ ??
+				end loop;
 			end if;
 		end if;
 	end process;
@@ -80,11 +87,12 @@ begin
 	begin
 		if rising_edge(clk) then
 			if sresetn = '0' then
-				state_req   <= UPDATING;
+				k_req     <= (others => '0');
+				state_req <= UPDATING;
 			elsif enable = '1' then
 				case state_req is
 					when UPDATING =>
-						k_req <= k_req_t(N-1); -- Key update
+						k_req <= k_req_t(N); -- Key update
 						state_req <= UPDATING;
 						if k_lr = '1' then
 							state_req <= FREEZE;
